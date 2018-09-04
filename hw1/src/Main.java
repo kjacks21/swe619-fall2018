52f3ad49e669918aebc4fe864ba4c36b180a8c3d
Greenwald <ngreenwald@mitre.org>
2018-09-03 22:07:47 -0400

    add asserts to tests

--- hw1/src/Main.java
+++ hw1/src/Main.java
@@ -7,18 +7,24 @@ public class Main {
         // return 2
         List<Integer> a = Arrays.asList(12, 25, 18, 8);
         List<Integer> b = Arrays.asList(6, 2, 3, 2);
-        System.out.println(FindPrimeFactor.findPrimeFactor(a, b));
+        Integer result1 = FindPrimeFactor.findPrimeFactor(a, b);
+        assert result1 == 2;
+        System.out.println(result1);
 
         // return 1
         // 0 has infinite factors on for integers
         a = Arrays.asList(12, 0, 18, 8);
         b = Arrays.asList(6, 2, 3, 2);
-        System.out.println(FindPrimeFactor.findPrimeFactor(a, b));
+        Integer result2 = FindPrimeFactor.findPrimeFactor(a, b);
+        assert result2 == 1;
+        System.out.println(result2);
 
         // return 1
         a = Arrays.asList(0, 0, 18, 8);
         b = Arrays.asList(6, 2, 3, 2);
-        System.out.println(FindPrimeFactor.findPrimeFactor(a, b));
+        Integer result3 = FindPrimeFactor.findPrimeFactor(a, b);
+        assert result3 == 1;
+        System.out.println(result3);
 
         // return NPE
 //        a = Arrays.asList(12, 25, null, 8);
