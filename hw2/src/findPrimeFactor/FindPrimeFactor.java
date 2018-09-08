9a9a1cf124c4cd008871a2c1393d9a904c4625c6
Greenwald <ngreenwald@mitre.org>
2018-09-08 13:39:28 -0400

    findPrimeFactor throws NPE for null a and/or b; added tests

---
+++ hw2/src/findPrimeFactor/FindPrimeFactor.java
@@ -0,0 +1,50 @@
+package findPrimeFactor;
+
+import java.util.List;
+
+public class FindPrimeFactor {
+
+    public static int findPrimeFactor (List<Integer> a, List<Integer> b) throws NullPointerException {
+        // Requires: a not null; b not null;
+        //           there is some index i where b[i] is
+        //           both prime and a factor of a[i]
+        //
+        // Effects: return the least index
+        //          at which b[i] is a prime factor of a[i]
+        // E.g. findPrimeFactor ([12, 25, 18, 8], [6, 2, 3, 2]) = 2
+        // (Note: 6 is a factor of 12, but is not prime,
+        //  and 2 is prime, but is not a factor of 25.  However,
+        //  3 is a prime factor of 18. Hence, index "2" is the correct
+        //  answer.  index "3" is not a possible answer, because the
+        //  third index is not the least index with the desired property.)
+        // Also note that a[] and b[] need not be of the same length.
+
+
+        for (int i = 0; i <= b.size(); i++) {
+            // check if either element is null
+            if (a.get(i) == null || b.get(i) == null) {
+                // do nothing
+            }
+
+            else if (isPrime(b.get(i)) && a.get(i) % b.get(i) == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean isPrime (int n) {
+        // values less than 2 are not prime
+        if (n < 2) {
+            return false;
+        }
+
+        // check integers less than or equal to the sqrt(n)
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ hw2/src/findPrimeFactor/Main.java
@@ -0,0 +1,63 @@
+package findPrimeFactor;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // return 2
+        List<Integer> a = Arrays.asList(12, 25, 18, 8);
+        List<Integer> b = Arrays.asList(6, 2, 3, 2);
+        Integer result1 = FindPrimeFactor.findPrimeFactor(a, b);
+        assert result1 == 2;
+        System.out.println(result1);
+
+        // return 1
+        // 0 has infinite factors on for integers
+        a = Arrays.asList(12, 0, 18, 8);
+        b = Arrays.asList(6, 2, 3, 2);
+        Integer result2 = FindPrimeFactor.findPrimeFactor(a, b);
+        assert result2 == 1;
+        System.out.println(result2);
+
+        // return 1
+        a = Arrays.asList(0, 0, 18, 8);
+        b = Arrays.asList(6, 2, 3, 2);
+        Integer result3 = FindPrimeFactor.findPrimeFactor(a, b);
+        assert result3 == 1;
+        System.out.println(result3);
+
+        // return 1
+        a = Arrays.asList(9, 10);
+        b = Arrays.asList(null, 5);
+        Integer result4 = FindPrimeFactor.findPrimeFactor(a, b);
+        assert result4 == 1;
+        System.out.println(result4);
+
+        // return 1
+        a = Arrays.asList(null, 10);
+        b = Arrays.asList(null, 5);
+        Integer result5 = FindPrimeFactor.findPrimeFactor(a, b);
+        assert result5 == 1;
+        System.out.println(result5);
+
+        // null a returns NPE
+        a = null;
+        b = Arrays.asList(6, 2, 3, 2);
+        try { Integer result6 = FindPrimeFactor.findPrimeFactor(a, b); }
+        catch (NullPointerException e) {
+            System.out.println("NPE for null a, as expected");
+        }
+
+        // null b returns NPE
+        a = Arrays.asList(6, 2, 3, 2);
+        b = null;
+        try { Integer result6 = FindPrimeFactor.findPrimeFactor(a, b); }
+        catch (NullPointerException e) {
+            System.out.println("NPE for null b, as expected");
+        }
+
+    }
+
+}
