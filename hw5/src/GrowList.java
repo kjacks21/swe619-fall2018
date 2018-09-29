f25f1bcfb9d3b5dc09a3c9ee61c8641116be8244
Jackson, Kyle L <kljackson@mitre.org>
2018-09-28 23:34:01 -0400

    added inner class, still bugs

--- hw5/src/GrowList.java
+++ hw5/src/GrowList.java
@@ -1,53 +1,62 @@
 import java.util.*;
 //  GrowList is a mutable list that only gets longer.
-public class GrowList implements Iterable<E> {
+public class GrowList<E>{
 
     private Map<Integer,E> values;
 
-    public GrowList() { values = new HashMap<Integer,E>();}
+    public Iterator values () {return new GrowListGen(this);}
 
-    // add to the end of the list
-    public void add(E o) {
-        values.put(size(), o);
-    }
+//    public GrowList() { values = new HashMap<Integer,E>();}
 
-    // number of values in list
-    public int size() { return values.size(); }
-
-    // get ith value in list
-    public E get(int i) {
-        if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.get");
-        return values.get(i);
-    }
-
-    // update ith value in list;  return previous value or null if none
-    public E set(int i, E o) {
-        if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.set");
-        return values.put(i, o);
-    }
-
-    // private helper method
-    private boolean inRange(int i) { return (i >= 0) && (i < size()); }
-
-    public String toString() {
-        if (size() == 0) return "[]";
-        String result = "[";
-        for (int i = 0; i < size()-1; i++) {
-            result += values.get(i) + ",";
+    // inner class
+    private static class GrowListGen implements Iterator {
+        // add to the end of the list
+        public void add(E o) {
+            values.put(size(), o);
         }
-        return result + values.get(size() - 1) + "]";
-    }
 
-    public static void main(String[] args) {
-        GrowList<String> list = new GrowList<String>();
+        // number of values in list
+        public int size() {
+            return values.size();
+        }
 
-        System.out.println("list is:" + list);
-        list.add("cat");
-        System.out.println("list is:" + list);
-        list.add("dog");
-        System.out.println("list is:" + list);
-        list.set(1,"bat");
-        System.out.println("list is:" + list);
+        // get ith value in list
+        public E get(int i) {
+            if (!inRange(i)) throw new IndexOutOfBoundsException("GrowList.get");
+            return values.get(i);
+        }
 
+        // update ith value in list;  return previous value or null if none
+        public E set(int i, E o) {
+            if (!inRange(i)) throw new IndexOutOfBoundsException("GrowList.set");
+            return values.put(i, o);
+        }
+
+        // private helper method
+        private boolean inRange(int i) {
+            return (i >= 0) && (i < size());
+        }
+
+        public String toString() {
+            if (size() == 0) return "[]";
+            String result = "[";
+            for (int i = 0; i < size() - 1; i++) {
+                result += values.get(i) + ",";
+            }
+            return result + values.get(size() - 1) + "]";
+        }
+
+//    public static void main(String[] args) {
+//        GrowList<String> list = new GrowList<String>();
+//
+//        System.out.println("list is:" + list);
+//        list.add("cat");
+//        System.out.println("list is:" + list);
+//        list.add("dog");
+//        System.out.println("list is:" + list);
+//        list.set(1,"bat");
+//        System.out.println("list is:" + list);
+//
+//    }
     }
 }
