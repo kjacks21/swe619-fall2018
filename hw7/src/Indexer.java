a6eba7e5b173205f151ab68a0c1c29b5edefbfc7
Jackson, Kyle L <kljackson@mitre.org>
2018-10-21 19:56:29 -0400

    updated Indexer skeleton

--- hw7/src/Indexer.java
+++ hw7/src/Indexer.java
@@ -1,6 +1,6 @@
 public interface Indexer {
-    public Object searchable ();
-    // not sure if necessary
+//    public Object searchable ();
+//    // not sure if necessary
 
     public Object findElementInC (Object x);
     // find element of c
