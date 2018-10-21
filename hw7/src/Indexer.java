42ed3cc594610ffc206e3abf1dd107addb0c48c6
Jackson, Kyle L <kljackson@mitre.org>
2018-10-21 19:54:22 -0400

    updated skeleton

---
+++ hw7/src/Indexer.java
@@ -0,0 +1,10 @@
+public interface Indexer {
+    public Object searchable ();
+    // not sure if necessary
+
+    public Object findElementInC (Object x);
+    // find element of c
+
+    public Object sizeOfC (Object x);
+    // determine the size of c
+}
--- hw7/src/ListIndexer.java
+++ hw7/src/ListIndexer.java
@@ -1,9 +1,19 @@
-public class ListIndexer {
+public class ListIndexer implements Indexer {
     public static <E> int search (Indexer<E> c, E x) throws
-        NullPointerException, NotFoundException, ClassCastException {
+            NullPointerException, NotFoundException, ClassCastException {
         // EFFECTS: If c is null throws NullPointerException, else if
         // c is not searchable, throws ClassCastException, else
         // if x is in c returns an index where x can be found,
         // else throws NotFoundException
+    }
+
+    public Object findElementInC (Object x) {
+        // find element of c
+        return null;
+    }
+
+    public Object sizeOfC (Object x) {
+        // determine the size of c
+        return null;
     }
 }
