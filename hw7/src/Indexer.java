ecdd40de39802ca7965968da3a844ee11bc29589
Jackson, Kyle L <kljackson@mitre.org>
2018-10-22 21:27:24 -0400

    latest updates

--- hw7/src/Indexer.java
+++ hw7/src/Indexer.java
@@ -2,9 +2,15 @@ public interface Indexer {
 //    public Object searchable ();
 //    // not sure if necessary
 
-    public Object findElementInC (Object x);
+    public static <E> int search (Indexer<E> c, E x) throws
+            NullPointerException, IllegalArgumentException, ClassCastException {
+        // TODO implement
+        return 0;
+    }
+
+    public <E> findElementInC (E x) throws NullPointerException;
     // find element of c
 
-    public Object sizeOfC (Object x);
+    public <E> sizeOfC (E x) throws NullPointerException;
     // determine the size of c
 }
--- hw7/src/ListIndexer.java
+++ hw7/src/ListIndexer.java
@@ -1,6 +1,13 @@
+import java.util.*;
+
 public class ListIndexer implements Indexer {
+
+    private List<E> c;
+
+    public ListIndexer () { c = new ArrayList<E>(); }
+
     public static <E> int search (Indexer<E> c, E x) throws
-            NullPointerException, NotFoundException, ClassCastException {
+            NullPointerException, IllegalArgumentException, ClassCastException {
         // EFFECTS: If c is null throws NullPointerException, else if
         // c is not searchable, throws ClassCastException, else
         // if x is in c returns an index where x can be found,
