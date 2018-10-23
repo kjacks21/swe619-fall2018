0d7d7045aeda06c402b8a3c43eead48055ba9056
Jackson, Kyle L <kljackson@mitre.org>
2018-10-23 15:26:57 -0400

    spacing fixes

--- hw7/src/ListIndexerTest.java
+++ hw7/src/ListIndexerTest.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class ListIndexerTest<E> {
-     ListIndexer<String> l = new ListIndexer<String>();
+    ListIndexer<String> l = new ListIndexer<String>();
     IndexSearch s;
     static List<String> list;
 
@@ -20,40 +20,43 @@ public class ListIndexerTest<E> {
 		s= new IndexSearch(list);
 	}
 	
-	 @Test(expected = NullPointerException.class)
-	    public  void testSearch() {
-	    	IndexSearch.search(null, "lizard");
-	    }
+	@Test(expected = NullPointerException.class)
+    public  void testSearch() {
+        IndexSearch.search(null, "lizard");
+    }
 
-	    @Test(expected = IllegalArgumentException.class)
-	    public void testSearch2() {
-	        IndexSearch.search(l, "bat");
-	    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testSearch2() {
+        IndexSearch.search(l, "bat");
+    }
 
-	    @Test
-	    public void testSearch3() {
-	    	int idx=IndexSearch.search(l,"Lizard");
-	    	assertEquals(2,idx);
-	    }
+    @Test
+    public void testSearch3() {
+        int idx=IndexSearch.search(l,"Lizard");
+        assertEquals(2,idx);
+    }
+
     @Test
     public void testindexOf() {
     	int idx=l.indexOf(list, "cat");
     	assertEquals(0,idx);
     }
+
     @Test
     public void testSearchable() {
     	boolean val = l.searchable(new HashMap<String,String>());
     	assertFalse(val);
-    	
     }
+
     @Test
     public void testSearchable2() {
     	boolean val = l.searchable(new ArrayList<String>());
     	assertTrue(val);
     }
+
     @Test
-   public void testSizeOf() {
+    public void testSizeOf() {
 	   int size = l.sizeOf(list);
 	   assertEquals(3,size);
-   }
+    }
 }
