672bf0924f7edc7ff7f93984ac44d7535e6d4234
Stefan <lucasarts230@yahoo.com>
2018-10-23 00:12:14 -0400

    more updates

--- hw7/src/Indexer.java
+++ hw7/src/Indexer.java
@@ -8,7 +8,7 @@ public interface Indexer {
         return 0;
     }*/
 	// instead maybe public int indexOf(Collection<E> collection)
-   public int indexOf(Collection<E> collection) //throws NullPointerException;
+   public int indexOf(Collection<E> collection, E x) //throws NullPointerException;
     // find element of c
 
 	//public int sizeOf(Collection<E> collection)
--- hw7/src/ListIndexer.java
+++ hw7/src/ListIndexer.java
@@ -6,13 +6,13 @@ public class ListIndexer implements Indexer {
 
     public ListIndexer () { c = new ArrayList<E>(); }
 
-    public static <E> int search (Indexer<E> c, E x) throws
+   /* public static <E> int search (Indexer<E> c, E x) throws
             NullPointerException, IllegalArgumentException, ClassCastException {
         // EFFECTS: If c is null throws NullPointerException, else if
         // c is not searchable, throws ClassCastException, else
         // if x is in c returns an index where x can be found,
         // else throws NotFoundException
-    }
+    }*/
 
     public Object findElementInC (Object x) {
         // find element of c
