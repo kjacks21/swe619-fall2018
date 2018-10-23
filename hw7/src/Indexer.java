deda8b9719d1686b16d532dd90d4a632a376961c
Stefan <lucasarts230@yahoo.com>
2018-10-23 00:09:22 -0400

    updated code

--- hw7/src/Indexer.java
+++ hw7/src/Indexer.java
@@ -1,17 +1,17 @@
 public interface Indexer {
-//    public boolean searchable (Object c);
+    public boolean searchable (Object c);
 //    // not sure if necessary
 
-    public static <E> int search (Indexer<E> c, E x) throws
+    /*public static <E> int search (Indexer<E> c, E x) throws
             NullPointerException, IllegalArgumentException, ClassCastException {
         // TODO implement
         return 0;
-    }
+    }*/
 	// instead maybe public int indexOf(Collection<E> collection)
-    public <E> findElementInC (E x) throws NullPointerException;
+   public int indexOf(Collection<E> collection) //throws NullPointerException;
     // find element of c
 
 	//public int sizeOf(Collection<E> collection)
-    public <E> sizeOfC (E x) throws NullPointerException;
+    public int sizeOf(Collection<E> collection) //throws NullPointerException;
     // determine the size of c
 }
