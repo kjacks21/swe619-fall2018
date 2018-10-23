df1988ca553de212b56640af45493ad9b9708d47
Stefan <lucasarts230@yahoo.com>
2018-10-22 23:57:31 -0400

    updated Indexer and added Searchable class

--- hw7/src/Indexer.java
+++ hw7/src/Indexer.java
@@ -1,5 +1,5 @@
 public interface Indexer {
-//    public Object searchable ();
+//    public boolean searchable (Object c);
 //    // not sure if necessary
 
     public static <E> int search (Indexer<E> c, E x) throws
@@ -7,10 +7,11 @@ public interface Indexer {
         // TODO implement
         return 0;
     }
-
+	// instead maybe public int indexOf(Collection<E> collection)
     public <E> findElementInC (E x) throws NullPointerException;
     // find element of c
 
+	//public int sizeOf(Collection<E> collection)
     public <E> sizeOfC (E x) throws NullPointerException;
     // determine the size of c
 }
