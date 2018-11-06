00a825daf4b481b9db6c6d334cb7e341cad80f00
Jackson, Kyle L <kljackson@mitre.org>
2018-11-06 17:11:46 -0500

    initialize hw10

---
+++ hw10/src/IntSet.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class IntSet implements Cloneable {
+    private List<Integer> els;
+
+    @Override public boolean equals(Object obj) {  // Standard recipe
+        if (!(obj instanceof IntSet)) return false;
+
+        IntSet s = (IntSet) obj;
+        return false;
+    }
+
+    @Override public int hashCode() {
+        // see below
+        return 42;
+    }
+
+    public IntSet () { els = new ArrayList<Integer>(); }
+
+    private IntSet (List<Integer> list) { els = list; }
+
+    @Override public IntSet clone() {
+        return new IntSet (new ArrayList<Integer>(els));
+    }
+}
