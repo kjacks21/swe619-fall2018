99057725e00a9223c36954f047054461e33c9f36
Stefan <lucasarts230@yahoo.com>
2018-11-18 14:00:00 -0500

    more spacing updates

--- hw11/src/InstrumentedMap.java
+++ hw11/src/InstrumentedMap.java
@@ -20,16 +20,16 @@ public class InstrumentedMap<K,V> extends ForwardingMap<K,V>{
 	   public int hashCode()      		{ return s.hashCode();}
 	   public String  toString()      	{ return s.toString();}
 	  
-	   public void clear() {s.clear();}
+	   public void clear()              {s.clear();}
 	   public boolean isEmpty()			{ return s.isEmpty();}
 	   public int size() 				{ return s.size();}
 
 	   public void putAll(Map<? extends K,? extends V> m) 
 	   	                               { s.putAll(m);}
-	   public boolean containsKey(Object key)	     {return s.containsKey(key);}
+	   public boolean containsKey(Object key)        {return s.containsKey(key);}
 	   public boolean containsValue(Object value)    {return s.containsValue(value);}
 	   public V get(Object key)                      {return s.get(key);}
-	   public Set<K> keySet()	                     {return s.keySet();}
+	   public Set<K> keySet()                        {return s.keySet();}
 	   public Collection<V> values()                 {return s.values();}
 	   public Set<Entry<K, V>> entrySet()            {return s.entrySet();}
 }
