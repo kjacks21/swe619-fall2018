75dd59b23c7f445283b717e20ccdd023a892ae95
Greenwald <ngreenwald@mitre.org>
2018-11-19 15:10:04 -0500

    add test and update README for List

--- hw11/src/InstrumentedTest.java
+++ hw11/src/InstrumentedTest.java
@@ -2,8 +2,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.HashSet;
 
 public class InstrumentedTest {
@@ -34,6 +39,49 @@ public class InstrumentedTest {
         // symmetry
         assertFalse(h.equals(t));
         assertFalse(t.equals(h));
+    }
+
+
+    InstrumentedList<String> a;
+    InstrumentedList<String> b;
+    InstrumentedList<String> c;
+
+    @Before
+    public void init2() {
+        a = new InstrumentedList<String>(new ArrayList<>());
+        b = new InstrumentedList<String>(new LinkedList<>());
+        c = new InstrumentedList<String>(new ArrayList<>());
+
+        a.add("Dog");
+        a.add("Cat");
+
+        b.add("Dog");
+        b.add("Cat");
+
+        c.add("Dog");
+        c.add("Cat");
+    }
+
+    @Test
+    public void testListEqualsReflexive() {
+        //reflexive
+        assertTrue(a.equals(a));
+
+    }
+
+    @Test
+    public void testListEqualsSymmetric() {
+        // symmetry
+        assertTrue(a.equals(b));
+        assertTrue(b.equals(a));
+    }
+
+    @Test
+    public void testListEqualsTransitive() {
+        // transitivity
+        assertTrue(a.equals(b));
+        assertTrue(b.equals(c));
+        assertTrue(a.equals(c));
     }
 
 }
--- hw11/src/README.txt
+++ hw11/src/README.txt
@@ -9,3 +9,8 @@ InstrumentedSet. The equals method in the Map interface calls the objects
 entrySet method as well as its own. This method creates a Set of the mappings
 for each. It then calls equals on those two Sets. It wouldn't matter if the object 
 is a Hashmap or a Treemap, because of the creation of the two Sets.
+
+There is no problem with the equals method in the list implementation. The equals method in the List interface
+calls the object's equals method. The List interface method defines two lists to be equal
+if they contain the same elements in the same order. It doesn't matter if the two lists being compared have
+different implementations, as long as they implement the List interface.
