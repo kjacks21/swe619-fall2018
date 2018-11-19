6607c52f11cdb60f31bab0f03ba88438ca42d0fd
Jackson, Kyle L <kljackson@mitre.org>
2018-11-18 20:08:00 -0500

    break collection equals tests apart

--- hw11/src/InstrumentedTest.java
+++ hw11/src/InstrumentedTest.java
@@ -2,31 +2,38 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 
 import java.util.HashSet;
 
 public class InstrumentedTest {
+    InstrumentedCollection<String> h;
+    InstrumentedCollection<String> t;
 
-	@Test
-	public void testEquals() {
-		InstrumentedCollection<String> h = new InstrumentedCollection<>(new HashSet<>());
-        InstrumentedCollection<String> t = new InstrumentedCollection<>(new HashSet<>());
+    @Before
+    public void init() {
+        h = new InstrumentedCollection<>(new HashSet<>());
+        t = new InstrumentedCollection<>(new HashSet<>());
 
         h.add("Dog");
         h.add("Cat");
 
         t.add("Dog");
         t.add("Cat");
+    }
 
-        // fails both
-
+	@Test
+	public void testCollectionEqualsReflexive() {
 		//reflexive
 		assertTrue(h.equals(h));
 
-		// symmetry
-		assertTrue(h.equals(t));
-		assertTrue(t.equals(h));
-
 	}
+
+	@Test
+    public void testCollectionEqualsSymmetric() {
+        // symmetry
+        assertTrue(h.equals(t));
+        assertTrue(t.equals(h));
+    }
 
 }
