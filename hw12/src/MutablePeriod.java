058678d9f812a02b6e9c8f0af5a8951ca4dad0a5
Jackson, Kyle L <kljackson@mitre.org>
2018-11-23 23:44:10 -0500

    hw12.3 init

---
+++ hw12/src/MutablePeriod.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.Date;
+
+public class MutablePeriod {
+    // A period instance
+    public final Period period;
+
+    // period's start field, to which we shouldn't have access
+    public final Date start;
+
+    // period's end field, to which we shouldn't have access
+    public final Date end;
+
+    public MutablePeriod() {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(bos);
+
+            // Serialize a valid Period instance
+            out.writeObject(new Period(new Date(), new Date()));
+
+            byte[] ref = { 0x71, 0, 0x7e, 0, 5 };
+            bos.write(ref); // The start field
+            ref[4] = 4;
+            bos.write(ref); // The end field
+
+            // Deserialize Period and "stolen" Date references
+            ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bos.toByteArray()));
+            period = (Period) in.readObject();
+            start = (Date) in.readObject();
+            end = (Date) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        MutablePeriod mp = new MutablePeriod();
+        Period p = mp.period;
+        Date pEnd = mp.end;
+
+        // Let's turn back the clock
+        pEnd.setYear(78);
+        System.out.println(p);
+
+        // Bring back the 60s!
+        pEnd.setYear(69);
+        System.out.println(p);
+    }
+}
